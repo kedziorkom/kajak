c3b82c4d3ebe21246d7dd8b1413894f9db838b2f
kedziorkom <kedziorkom.east@gmail.com>
2019-11-02 22:26:25 +0100

    commit1

--- kajak/MainActivity.cs
+++ kajak/MainActivity.cs
@@ -3,24 +3,95 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using static Android.Views.View;
+using Android.Views;
 
 namespace kajak
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : Activity, IOnClickListener, IOnCompleteListener
     {
+        Button btnLogin;
+        EditText input_email, input_password;
+        TextView btnSignUp, btnForgotPassword;
+
+        RelativeLayout activity_main;
+
+        public static FirebaseApp app;
+        FirebaseAuth auth;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
-        }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            //StartActivity(new Android.Content.Intent(this, typeof(tracker)));
 
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            SetContentView(Resource.Layout.activity_main);
+
+            //Init Firebase
+            InitFirebaseAuth();
+
+            //View
+            btnLogin = FindViewById<Button>(Resource.Id.login_btn_login);
+            input_email = FindViewById<EditText>(Resource.Id.login_email);
+            input_password = FindViewById<EditText>(Resource.Id.login_password);
+            btnSignUp = FindViewById<TextView>(Resource.Id.login_btn_sign_up);
+            btnForgotPassword = FindViewById<TextView>(Resource.Id.login_btn_forget_password);
+            activity_main = FindViewById<RelativeLayout>(Resource.Id.activity_main);
+
+            btnSignUp.SetOnClickListener(this);
+            btnLogin.SetOnClickListener(this);
+            btnForgotPassword.SetOnClickListener(this);
+        }
+
+        private void InitFirebaseAuth()
+        {
+            var options = new FirebaseOptions.Builder()
+            .SetApplicationId("1:68142252041:android:a3236c3263470ef7")
+            .SetApiKey("AIzaSyBDL59DxShfWhRtIH4hXFMMJrrhM8WNQ8c")
+            .Build();
+
+            if (app == null)
+                app = FirebaseApp.InitializeApp(this, options);
+            auth = FirebaseAuth.GetInstance(app);
+        }
+
+        public void OnClick(View v)
+        {
+            if (v.Id == Resource.Id.login_btn_forget_password)
+            {
+                StartActivity(new Android.Content.Intent(this, typeof(ForgetPassword)));
+                Finish();
+            }
+            else if (v.Id == Resource.Id.login_btn_sign_up)
+            {
+                StartActivity(new Android.Content.Intent(this, typeof(SignUp)));
+                Finish();
+            }
+            else if (v.Id == Resource.Id.login_btn_login)
+            {
+                LoginUser(input_email.Text, input_password.Text);
+            }
+        }
+
+        private void LoginUser(string email, string password)
+        {
+            auth.SignInWithEmailAndPassword(email, password)
+                .AddOnCompleteListener(this);
+        }
+
+        public void OnComplete(Task task)
+        {
+            if (task.IsSuccessful)
+            {
+                StartActivity(new Android.Content.Intent(this, typeof(MainMenu)));
+                Finish();
+            }
+            else
+            {
+                Snackbar snackBar = Snackbar.Make(activity_main, "Logowanie nie powiodło się ", Snackbar.LengthShort);
+                snackBar.Show();
+            }
         }
     }
 }
